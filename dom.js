a3e786c742e109154df412ef8cd5d52902189300
amar-nath1 <amarat279955>
2022-10-03 17:18:21 +0530

    Deliverable 2.1

--- dom.js
+++ dom.js
@@ -1,5 +1,3 @@
 
-let li2= document.querySelector('.list-group-item:nth-child(2)')
-li2.style.backgroundColor='green'
-let li3=document.querySelector('.list-group-item:nth-child(3)')
-li3.style.color='white'
+let li2= document.querySelectorAll('.list-group-item')
+li2[1].style.color='green'
