9ca8ec6f16ca4b36433dfaa04aa13ea6ce257104
amar-nath1 <amarat279955>
2022-10-03 17:05:32 +0530

    adding for qs

---
+++ dom.js
@@ -0,0 +1,5 @@
+
+let li2= document.querySelector('.list-group-item:nth-child(2)')
+li2.style.backgroundColor='green'
+let li3=document.querySelector('.list-group-item:nth-child(3)')
+li3.style.color='white'
