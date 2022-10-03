64560bd4150f14f6aaa09f820545778a5ef8a964
amar-nath1 <amarat279955>
2022-10-03 17:25:11 +0530

    deliverable 2.2

--- dom.js
+++ dom.js
@@ -1,3 +1,8 @@
 
 let li2= document.querySelectorAll('.list-group-item')
 li2[1].style.color='green'
+let oddli = document.querySelectorAll('.list-group-item:nth-child(odd)')
+for (i=0;i<oddli.length;i++){
+
+    oddli[i].style.backgroundColor='green'
+}
